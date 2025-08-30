8098555239e787b121be7db742dd9394cb9d67b0
EpicMumble <125847901+EpicMumble@users.noreply.github.com>
2025-08-30 13:42:22 -0400

    Add files via upload

---
+++ PythonPractice1.py
@@ -0,0 +1,9 @@
+
+name = input("what is your name?") ; 
+country = input("what country do you live in?")
+country = country.upper()
+
+print("\nHello," + name + ". You live in " + country) 
+if country == "USA" :
+    print ("In 1776 was when the Declaration of Independance was signed")
+
